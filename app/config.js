f807c9547eac65e68acf40c8915a2454ffb4145f
guilhermeanom-crypto <guilherme.anom@gmail.com>
2026-04-29 11:17:53 -0800

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 window.HABILIS_CAPTACAO_CONFIG = {
   apiBaseUrl: "https://cixgnglubgczawnfyspw.supabase.co/functions/v1",
   publicLeadEndpoint: "bright-responder",
-  turnstileSiteKey: "",
+  turnstileSiteKey: "SUA_SITE_KEY_AQUI",
   simulateWhenOffline: false,
 };
